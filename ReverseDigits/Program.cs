3e1f4d8b932886223fecc1b06c129e9d182044a4
wwstudent <wwstudent@MS-9D12CH2>
2019-08-01 11:40:24 -0700

    New Project

---
+++ ReverseDigits/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ReverseDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(reverseInstructor(-90856));
+        }
+        public static int reverseDigits(int a)
+        {
+            bool negative = (a < 0);
+            int reversed = 0;
+            a *= (negative) ? -1 : 1;
+            while (a > 0)
+            {
+                int digit = a % 10;
+                if(reversed > 0)
+                {
+                    reversed *= 10;
+                }
+                reversed += digit;
+                a /= 10;
+            }
+            reversed *= (negative) ? -1 : 1;
+
+            return reversed;
+        }
+        public static int reverseInstructor(int x)
+        {
+            bool isNeg = (x < 0);
+            int sum = 0;
+            x *= isNeg ? -1 : 1;
+            while (x>0)
+            {
+                sum *= 10;
+                int remainder = x % 10;
+                sum += remainder;
+                x /= 10;
+            }
+            sum *= isNeg ? -1 : 1;
+            return sum;
+        }
+    }
+}
---
+++ ReverseInt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            reverseInt(2, 4);
+        }
+        public static void reverseInt(int a, int b)
+        {
+            b = a + b;
+            a = b - a;
+            b = b - a;
+            Console.WriteLine($"{ a},{ b}");
+        }
+    }
+}
